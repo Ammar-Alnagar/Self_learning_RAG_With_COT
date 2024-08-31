f50a8ecbfe9efc3a5b48f683f35deb3057dfbafd
Ammar <76597039+Ammar-Alnagar@users.noreply.github.com>
2024-09-01 02:05:13 +0300

    Create Indexer.py

---
+++ Indexer.py
@@ -0,0 +1,39 @@
+# from langchain_experimental.text_splitter import SemanticChunker # type: ignore
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+
+
+from langchain_community.document_loaders import DirectoryLoader
+from langchain_community.embeddings import OllamaEmbeddings
+from langchain_community.vectorstores import Chroma
+
+# Load documents from a PDF file
+loader = DirectoryLoader("./data", glob="**/*.pdf")
+
+print("pdf loaded loader")
+
+documents = loader.load()
+
+print(len(documents))
+
+# # Create embeddingsclear
+embeddings = OllamaEmbeddings(model="nomic-embed-text", show_progress=True)
+
+# # Create Semantic Text Splitter
+# text_splitter = SemanticChunker(embeddings, breakpoint_threshold_type="interquartile")
+
+text_splitter = RecursiveCharacterTextSplitter(
+    chunk_size=1500,
+    chunk_overlap=300,
+    add_start_index=True,
+)
+
+# # Split documents into chunks
+texts = text_splitter.split_documents(documents)
+
+# # Create vector store
+vectorstore = Chroma.from_documents(
+    documents=texts, 
+    embedding= embeddings,
+    persist_directory="./db-mawared")
+
+print("vectorstore created")
