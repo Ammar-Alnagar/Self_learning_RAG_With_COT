74a01f7c299f5f4f5f39519be09a5efc38816cde
Ammar Alnagar <ammaralnagar416@gmial.com>
2024-10-02 18:16:46 +0300

    changed base brain model

--- Main_rag.py
+++ Main_rag.py
@@ -29,8 +29,8 @@ retriever = db.as_retriever(
 
 # Initialize the LLM (Groq's version of LLaMA)
 llm = ChatGroq(
-    model="llama-3.1-8b-instant",
-    temperature=0,        # Deterministic output
+    model="llama-3.1-70b-versatile",
+    temperature=0.3,        # Deterministic output
     max_tokens=None,
     timeout=None,
     max_retries=2
