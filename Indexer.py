9ea86497a947a1d6ded91db6dd1cee8e15942c23
Ammar <76597039+Ammar-Alnagar@users.noreply.github.com>
2024-10-02 14:00:02 +0300

    Update Indexer.py

--- Indexer.py
+++ Indexer.py
@@ -1,9 +1,9 @@
-# indexer.py
-
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.document_loaders import DirectoryLoader
 from langchain_community.embeddings import OllamaEmbeddings
 from langchain_community.vectorstores import Chroma
+
+
 
 
 
